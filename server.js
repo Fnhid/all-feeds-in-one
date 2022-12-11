9f61af69b55857576571ebfe969eaacad2811f67
Fnhid <fnhid05@gmail.com>
2022-12-11 21:22:22 +0900

    clean root dir

--- server.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')  
-const app = express()
-const router = require("./router/parser");
-app.use('/', router)
-
-app.listen(8080, () => {
-    console.log('listening on 8080');
-})
