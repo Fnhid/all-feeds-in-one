5fc30b21da8085be51849d613b7fbeef552b23f8
Fnhid <fnhid05@gmail.com>
2022-12-11 21:21:03 +0900

    get api based on rss.json

---
+++ server/parser.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const router = express.Router();
+{/* <script src="./rss.json" type="text/javascript"></script> */}
+let Parser = require('rss-parser');
+let parser = new Parser();
+const sites = require('./rss.json');
+
+
+const parseRSS = async (sites) => {
+
+    const parseContent = async (site) => {
+        
+        let feed = await parser.parseURL(site.url);
+        return (feed.items.map(item => {
+            return {
+                title: item.title,
+                link: item.link, 
+                date: item.pubDate,
+                siteId: site.id
+            }
+        })
+        )
+    }
+    let contents = await Promise.all(sites.map(function(site){
+        return parseContent(site);
+    }));
+
+    return {
+        "sites": sites,
+        "contents": contents
+    };
+} 
+
+
+
+router.get('/', async (req, res) => {
+    await parseRSS(sites.sites)
+        .then(data => {
+            res.status(200).json(data)
+        })
+        .catch(err => {
+            res.status(404).json({
+                status: 'error',
+                message: 'error occured'
+            })
+        })
+});
+
+module.exports = router;
